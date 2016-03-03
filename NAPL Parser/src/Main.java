8112ea89839c8113bde90c82c096989df14d3927
Nate Palmer <netpalm555@gmail.com>
2016-03-03 08:24:20 -0500

    Started on syntax checking and a help file for the parser

--- NAPL Parser/src/Main.java
+++ NAPL Parser/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main (String[] args) {
-
+        for(int i = 0; i  < args.length; i++) {
+            if(i == 0) {
+                switch (args[i]) {
+                    case "-h":
+                        break;
+                }
+            }
+        }
     }
 
 }
---
+++ NAPL Parser/src/SyntaxChecker.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class SyntaxChecker {
+
+    File fileToCheck;
+
+    public SyntaxChecker(String filepath) {
+        fileToCheck = new File(filepath);
+    }
+
+
+
+}
