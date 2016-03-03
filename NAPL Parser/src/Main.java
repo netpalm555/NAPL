c84873abd1db7ccf05bbb1430a506842b9a193f9
Nate Palmer <netpalm555@gmail.com>
2016-03-03 15:09:27 +0000

    Correctly commit everything

--- NAPL Parser/src/Main.java
+++ NAPL Parser/src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main (String[] args) {
         for(int i = 0; i  < args.length; i++) {
+            System.out.println(args[i]);
             if(i == 0) {
                 switch (args[i]) {
                     case "-h":
+                    case "--help":
+                        System.out.println("This will be the help file for the NAPL parser.");
                         break;
                 }
             }
